fe02604a3b503b391c8747a8904cbaeabac367ef
fatezc <1157440840@qq.com>
2019-09-16 16:53:24 +0800

    980.不同路径_3,回溯法dfs

---
+++ algorithm_github/src/practise/FindPath_3.java
@@ -0,0 +1,77 @@
+package practise;
+/*
+* 在二维网格 grid 上，有 4 种类型的方格：
+
+1 表示起始方格。且只有一个起始方格。
+2 表示结束方格，且只有一个结束方格。
+0 表示我们可以走过的空方格。
+-1 表示我们无法跨越的障碍。
+返回在四个方向（上、下、左、右）上行走时，从起始方格到结束方格的不同路径的数目，每一个无障碍方格都要通过一次。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/unique-paths-iii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+* */
+/*
+先找到开始和结束的位置,记录为sr,sc和tr,tc,同时记录grid中0的个数记录为todo
+然后采用回溯法,从起始位置开始dfs
+由于dfs过程中开始和结束位置的值会变,所以不能由grid[r][c]==2来判断结束,而是用记录的tr和tc
+ */
+public class FindPath_3 {
+    int ans;
+    int[][] grid;
+    int tr, tc;
+    int[] dr = new int[]{0, -1, 0, 1};
+    int[] dc = new int[]{1, 0, -1, 0};
+    int R, C;
+
+    public int uniquePathsIII(int[][] grid) {
+        this.grid = grid;
+        R = grid.length;
+        C = grid[0].length;
+
+        int todo = 0;
+        int sr = 0, sc = 0;
+        for (int r = 0; r < R; ++r)
+            for (int c = 0; c < C; ++c) {
+                if (grid[r][c] != -1) {
+                    todo++;
+                }
+
+                if (grid[r][c] == 1) {
+                    sr = r;
+                    sc = c;
+                } else if (grid[r][c] == 2) {
+                    tr = r;
+                    tc = c;
+                }
+            }
+
+        ans = 0;
+        dfs(sr, sc, todo);
+        return ans;
+    }
+
+    public void dfs(int r, int c, int todo) {
+        todo--;
+        if (todo < 0) return;
+        if (r == tr && c == tc) {
+            if (todo == 0) ans++;
+            return;
+        }
+
+        grid[r][c] = 3;
+        //下面这个循环为对(r,c)的上下左右四个dfs
+        for (int k = 0; k < 4; ++k) {
+            int nr = r + dr[k];
+            int nc = c + dc[k];
+            if (0 <= nr && nr < R && 0 <= nc && nc < C) {
+                if (grid[nr][nc] % 2 == 0)
+                    dfs(nr, nc, todo);
+            }
+        }
+        grid[r][c] = 0;
+    }
+
+
+}
