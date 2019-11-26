c5ab02f01a61c920cb7dc754f713e0a68bb15137
zhang_c <1157440840@qq.com>
2019-11-26 18:03:49 +0800

    1139.最大的以1为边界的正方形

--- algorithm_github/src/practise/Largest1BorderedSquare.java
+++ algorithm_github/src/practise/Largest1BorderedSquare.java
@@ -15,7 +15,7 @@ package practise;
 * */
 public class Largest1BorderedSquare {
     public int largest1BorderedSquare(int[][] grid) {
-        //dp[i][j][0]记录每个点朝上最多有多少个连着的1，dp[i][j][1]记录朝左最多有多少个连着的1
+        //dp[i][j][0]记录每个点朝上最多有多少个连着的1，dp[i][j][1]记录朝左最多有多少个连着的1，如示例1：dp[2][2][0] = 3,dp[2][2][1] = 3
         int[][][] dp = new int[grid.length][grid[0].length][2];
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
@@ -30,9 +30,8 @@ public class Largest1BorderedSquare {
             for (int j = 0; j < grid[0].length; j++) {
                 if (Math.min(dp[i][j][0],dp[i][j][1])<=ans)
                     continue;
-                //对于每一个可能改变ans的点，将它作为正方形的右下角的端点，检查正方形的四条边
-                int help = Math.min(dp[i][j][0],dp[i][j][1]);
-                for (;help>=ans;help--){
+                //对于每一个可能改变ans的点，将它作为正方形的右下角的端点,检查正方形的另外两条边
+                for (int help = Math.min(dp[i][j][0],dp[i][j][1]);help>=ans;help--){
                    if (dp[i][j-help+1][0] >= help && dp[i-help+1][j][1]>=help){
                        ans = help;
                        break;
